c34806bd325755ec74e861bd3384413a7bfaa0db
Hadil Friaa <hadil@MacBook-Pro-de-Hadil.local>
2021-11-19 10:33:53 +0100

    tests unitaires

---
+++ ArchiLog/TestsUnitaires/MockDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Archi.api.Data;
+using Archi.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestProject1
+{
+    public class MockDbContext : ArchiDbContext
+    {
+        public MockDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public static MockDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase("dbmok").Options;//détermine les options de connexion pour al base en mémoire
+            // DbContextOptionsBuilder initialise une nouvelle instance de Microsoft.EntityFrameworkCore.DbContextOptionsBuilder (classe sans aucune option définie)
+            // UseInMemoryDatabase configure le contexte pour se connecter à une base de données en mémoire nommée.
+            var db = new MockDbContext(options);
+            db.Pizzas.Add(new Pizza { ID = 1, Active = true, Name = "Pizza1", Price = 10 }); //Ajoute des pizzas à la base de données mock
+            db.Pizzas.Add(new Pizza { ID = 2, Active = true, Name = "Pizza2", Price = 15 });
+            db.Pizzas.Add(new Pizza { ID = 3, Active = false, Name = "Pizza3", Price = 15 });
+            db.Pizzas.Add(new Pizza { ID = 4, Active = true, Name = "Pizza4", Price = 15 });
+            db.Pizzas.Add(new Pizza { ID = 5, Active = true, Name = "Pizza5", Price = 15 });
+            db.SaveChanges();
+            return db;
+        }
+    }
+}
--- ArchiLog/TestsUnitaires/UnitTest1.cs
+++ ArchiLog/TestsUnitaires/UnitTest1.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Archi.api.Data;
+using Archi.api.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Archi.api.Controllers;
+using Microsoft.EntityFrameworkCore;
 
-namespace TestsUnitaires
+
+namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly ArchiDbContext repository;
+
+        public UnitTest1()
         {
+            repository = MockDbContext.GetDbContext();
+        }
+
+        
+
+
+        [TestMethod]
+        public void GetIdTest()
+        {
+            var controller = new PizzasController(repository); //récupère les méthodes de PizzasController avec les données du mock
+            var pizza = controller.GetPizza(1).Result.Value; //Utilise la méthode GetPizza, result car méthode async, récupère sa valeur avec value
+            Assert.IsNotNull(pizza); // vérifie si la nouvelle pizza est nulle
+            var result = controller.PostPizza(new Pizza { ID = 20 }).Result; // utilise la méthode PostPizza du controller et on créé une nouvelle pizza
+            var pizza2 = repository.Pizzas.SingleOrDefaultAsync(x => x.ID == 20).Result; // on va récupérer les valeurs de la base de données mock avec l'Id qui est égal à deux
+            Assert.IsNotNull(pizza); //On vérifie que la pizza n'est pas nulle
+
+
+
+            //var controllerCustomer = new CustomersController(repository); //récupère les méthodes de CustomersController avec les données du mock
+
         }
     }
 }
